bcff5980252694bcce9ae7c30ad2d1e63784b3ab
Ashutosh Gangwar <ashutosh@nilenso.com>
2023-08-18 12:14:30 +0530

    add samples from Chapter 2 of the Joy of Clojure

--- src/clojure_sandbox/chapter_2.clj
+++ src/clojure_sandbox/chapter_2.clj
@@ -12,7 +12,7 @@
 (add 1 2 3 4 5)
 
 
-;; a block will only produce the value of the last expression.(
+;; a block will only produce the value of the last expression.
 (do
   (+ 1 2)
   (+ 2 3)
@@ -27,7 +27,7 @@
 
 ;; tail recursion
 (defn print-range [x y]
-  (when (<= x y)
+  (when (<= x y) ; use `when` when the else block is not needed.
     (println x)
     (recur (inc x) y)))
 
@@ -40,3 +40,63 @@
       sum)))
 
 (sum-range 1 4)
+
+;; quote simply prevents evaluation
+'(+ 2 3 4)
+
+;; syntax-quote also qualifies unqualified symbols while preventing evaluation.
+`(+ 2 3 4)
+
+;; unquote `~` is used to denote any Clojure expression as requiring evaluation.
+`(+ 10 ~(* 3 2))
+
+;; but it doesn't work with a regular quote.
+'(+ 10 ~(* 3 2))
+
+;; like unquote, except the result is spliced.
+`(+ 10 ~@[1 2 3])
+
+;; using Java static members
+(Math/sqrt 2)
+
+;; constructing Java objects
+(new java.awt.Point 1 1)
+
+;; or
+(java.awt.Point. 1 1)
+
+;; referencing public fields
+(.-x (java.awt.Point. 1 1))
+
+;; invoking public methods
+(.concat "abc" "def")
+
+;; setting public fields
+(let [origin (java.awt.Point. 0 0)]
+  (set! (.-x origin) 15)
+  (str origin))
+
+;; .. macro
+(.. (StringBuilder.) (append "abc") (append "def") (append "ghi") (toString))
+
+;; or using thread-first macro `->`.
+(-> (StringBuilder.)
+    (.append "abc")
+    (.append "def")
+    (.append "ghi")
+    (.toString))
+
+;; or using `doto` macro, but instead of producing the value of the last expression,
+;; it will produce the built object, i.e. a `StringBuilder` in this case.
+(doto (StringBuilder.)
+  (.append "abc")
+  (.append "def")
+  (.append "ghi")
+  (.toString))
+
+
+;; try, throw, catch and finally.
+(try
+  (throw (Exception. "test-error"))
+  (catch Exception e (.getMessage e))
+  (finally (println "test-finally")))
