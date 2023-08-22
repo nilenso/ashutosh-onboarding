73c3fb5d0e8c80d473dcdee5aba028c8d7d14b01
Ashutosh Gangwar <ashutosh@nilenso.com>
2023-08-22 12:51:58 +0530

    add solutions to problems 39-45 from 4Clojure

--- src/clojure_sandbox/4clojure.clj
+++ src/clojure_sandbox/4clojure.clj
@@ -184,7 +184,7 @@
 ;; 33. Write a function which replicates each element of a sequence a variable
 ;; number of times.
 (defn replicate-n-times [items times]
-  (reduce #(concat %1 (take times (repeat %2))) '() items))
+  (reduce #(concat %1 (repeat times %2)) '() items))
 
 (replicate-n-times [1 2 3] 2)
 (replicate-n-times [:a :b] 4)
@@ -213,3 +213,88 @@
 (max* 1 8 3 4)
 (max* 30 20)
 (max* 45 67 11)
+
+;; 39. Write a function which takes two sequences and returns the first item
+;; from each, then the second item from each, then the third, etc.
+(defn interleave* [items-1 items-2]
+  (loop [items-1 items-1, items-2 items-2, interleaved (empty items-1)]
+    (if (and (seq items-1) (seq items-2))
+      (recur
+       (rest items-1)
+       (rest items-2)
+       (conj interleaved (first items-1) (first items-2)))
+      interleaved)))
+
+(interleave* [1 2 3] [:a :b :c])
+(interleave* [1 2] [3 4 5 6])
+(interleave* [1 2 3 4] [5])
+(interleave* [30 20] [25 15])
+
+;; 40. Write a function which separates the items of a sequence by an arbitrary
+;;     value.
+(defn interpose* [separator items]
+  (pop (reduce #(conj %1 %2 separator) [] items)))
+
+(interpose* 0 [1 2 3])
+(apply str (interpose* ", " ["one" "two" "three"]))
+(interpose* :z [:a :b :c :d])
+
+;; 41. Write a function which drops every Nth item from a sequence.
+(defn drop-nth [items n]
+  (loop [[current & remaining :as items] items, index 1, result []]
+    (if items
+      (recur
+       remaining
+       (inc index)
+       (if (= 0 (mod index n))
+         result
+         (conj result current)))
+      result)))
+
+(drop-nth [1 2 3 4 5 6 7 8] 3)
+(drop-nth [:a :b :c :d :e :f] 2)
+(drop-nth [1 2 3 4 5 6] 4)
+
+;; 42. Write a function which calculates factorials.
+(defn factorial [n]
+  (reduce #(* %1 (inc %2)) 1 (range n)))
+
+(factorial 1)
+(factorial 3)
+(factorial 5)
+(factorial 8)
+
+;; 43. Write a function which reverses the interleave process into x number of
+;;     subsequences.
+(defn reverse-interleave [items layer-count]
+  (loop [[current & remaining :as items] items
+         index 0
+         result (vec (repeat layer-count []))]
+    (if items
+      (recur
+       remaining
+       (inc index)
+       (let [current-seq-index (mod index layer-count)]
+         (update result current-seq-index conj current)))
+      result)))
+
+(reverse-interleave [1 2 3 4 5 6] 2)
+(reverse-interleave (range 9) 3)
+(reverse-interleave (range 10) 5)
+
+;; 44. Write a function which can rotate a sequence in either direction.
+(defn rotate [n items]
+  (let [c (count items)
+        n (if (> n 0)
+            (mod n c)
+            (- c (mod (abs n) c)))]
+    (concat (drop n items) (take n items))))
+
+(rotate 2 [1 2 3 4 5])
+(rotate -2 [1 2 3 4 5])
+(rotate 6 [1 2 3 4 5])
+(rotate 1 '(:a :b :c))
+(rotate -4 '(:a :b :c))
+
+;; 45. The iterate function can be used to produce an infinite lazy sequence.
+(take 5 (iterate #(+ 3 %) 1))
