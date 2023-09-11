1bfad0db7a4f3499c6f8ee94b7faf52f1d309ff5
Ashutosh Gangwar <ashutosh@nilenso.com>
2023-09-11 15:32:36 +0530

    add remaining code samples from Chapter 15 of JoC

--- clojure-sandbox/src/clojure_sandbox/chapter_15.clj
+++ clojure-sandbox/src/clojure_sandbox/chapter_15.clj
@@ -1,4 +1,5 @@
-(ns clojure-sandbox.chapter-15)
+(ns clojure-sandbox.chapter-15
+  (:require [clojure.core.reducers :as r]))
 
 (defn asum-sq ^Long [^ints xs]
   (let [^ints sq-xs (amap xs i _ (* (aget xs i) (aget xs i)))]
@@ -83,3 +84,161 @@
 (factorial-e 30.0)
 (factorial-e 171.0)
 (time (dotimes [_ 1e5] (factorial-e 20.0)))
+
+(defprotocol CacheProtocol
+  (lookup  [cache e])
+  (has?    [cache e])
+  (hit     [cache e])
+  (miss    [cache e ret]))
+
+(deftype BasicCache [cache]
+  CacheProtocol
+  (lookup [_ item]
+    (get cache item))
+  (has? [_ item]
+    (contains? cache item))
+  (hit [this item] this)
+  (miss [_ item result]
+    (BasicCache. (assoc cache item result))))
+
+(def cache (BasicCache. {}))
+
+(lookup (miss cache '(servo) :robot) '(servo))
+
+(defn through [cache f item]
+  (if (has? cache item)
+    (hit cache item)
+    (miss cache item (delay (apply f item)))))
+
+(deftype PluggableMemoization [f cache]
+  CacheProtocol
+  (has? [_ item] (has? cache item))
+  (hit  [this item] this)
+  (miss [_ item result]
+    (PluggableMemoization. f (miss cache item result)))
+  (lookup [_ item]
+    (lookup cache item)))
+
+(defn memoization-impl [cache-impl]
+  (let [cache (atom cache-impl)]
+    (with-meta
+      (fn [& args]
+        (let [cs (swap! cache through (.f cache-impl) args)]
+          @(lookup cs args)))
+      {:cache cache})))
+
+(def slowly (fn [x] (Thread/sleep 1500) x))
+(def sometimes-slowly (memoization-impl
+                       (PluggableMemoization.
+                        slowly
+                        (BasicCache. {}))))
+
+(time [(sometimes-slowly 108) (sometimes-slowly 108)])
+(time [(sometimes-slowly 108) (sometimes-slowly 108)])
+
+(defn empty-range? [start end step]
+  (or (and (pos? step) (>= start end))
+      (and (neg? step) (<= start end))))
+
+(defn lazy-range [start end step]
+  (lazy-seq
+   (if (empty-range? start end step)
+     nil
+     (cons start (lazy-range (+ start step) end step)))))
+
+
+(lazy-range 1 10 2)
+(reduce + 0 (lazy-range 1 100 2))
+
+
+;; returns `fn [reducing-fn init]`
+(defn reducible-range [start end step]
+  (fn [reducing-fn init]
+    (loop [result init
+           i start]
+      (if (empty-range? i end step)
+        result
+        (recur (reducing-fn result i) (+ i step))))))
+
+((reducible-range 1 100 2) + 0)
+
+(defn half [n]
+  (/ n 2))
+
+(defn sum-half [result input]
+  (+ result (half input)))
+
+(reduce sum-half 0 (lazy-range 1 100 2))
+((reducible-range 1 100 2) sum-half 0)
+
+(defn half-transformer [fn1]
+  (fn fn1-half [result input]
+    (fn1 result (half input))))
+
+((reducible-range 0 10 2) (half-transformer +) 0)
+((reducible-range 0 10 2) (half-transformer conj) [])
+
+(defn mapping [map-fn]
+  (fn map-transformer [f1]
+    (fn [result input]
+      (f1 result (map-fn input)))))
+
+((reducible-range 0 10 2) ((mapping half) +) 0)
+
+(defn mapcatting [map-fn]
+  (fn [f1]
+    (fn [result input]
+      (let [reducible (map-fn input)]
+        (reducible f1 result)))))
+
+(defn and-plus-ten [x]
+  (reducible-range x (+ 11 x) 10))
+
+((and-plus-ten 5) conj [])
+
+((reducible-range 0 10 2) ((mapcatting and-plus-ten) conj) [])
+
+(defn filtering [filter-pred]
+  (fn [f1]
+    (fn [result input]
+      (if (filter-pred input)
+        (f1 result input)
+        result))))
+
+(defn r-map [mapping-fn reducible]
+  (fn new-reducible [reducing-fn init]
+    (reducible ((mapping mapping-fn) reducing-fn) init)))
+
+(defn r-filter [filter-pred reducible]
+  (fn new-reducible [reducing-fn init]
+    (reducible ((filtering filter-pred) reducing-fn) init)))
+
+((reducible-range 0 10 2) ((filtering #(not= % 2)) +) 0)
+
+((r-filter #(not= % 2) (r-map half (reducible-range 0 10 2))) conj [])
+
+(require '[criterium.core :as crit])
+(crit/bench
+ (reduce + 0 (filter even? (map half (lazy-range 0
+                                                 (* 10 1000 1000) 2)))))
+
+(crit/bench
+ (reduce + 0 (filter even? (map half (range 0 (* 10 1000 1000) 2)))))
+
+(crit/bench
+ ((r-filter even? (r-map half
+                         (reducible-range 0 (* 10 1000 1000) 2))) + 0))
+
+(require '[clojure.core.reducers :as r])
+
+(defn core-r-map [mapping-fn core-reducible]
+  (r/reducer core-reducible (mapping mapping-fn)))
+
+(defn core-r-filter [filter-pred core-reducible]
+  (r/reducer core-reducible (filtering filter-pred)))
+
+(reduce conj []
+        (core-r-filter #(not= % 2)
+                       (core-r-map half [0 2 4 6 8])))
+
+(r/fold + [1 2 3 4 5])
