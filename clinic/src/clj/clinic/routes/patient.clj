fa5a0874d339062f8264917cb83e5bdf5c9d325c
Ashutosh Gangwar <ashutosh@nilenso.com>
2023-10-13 20:00:03 +0530

    clinic: add route handlers for front-end resources and  wrap-params middleware

--- clinic/src/clj/clinic/routes/patient.clj
+++ clinic/src/clj/clinic/routes/patient.clj
@@ -4,16 +4,21 @@
             [ring.util.response :as r]))
 
 (defn- create-patient! [{{fhir-server-url :fhir-server-base-url} :config
-                         params :body}]
-  (try
-    (-> (svc/create! fhir-server-url params)
-        (r/response)
-        (r/status 201))
-    (catch Exception e
-      (case (:type (ex-data e))
-        :invalid-params (r/status 400)
-        :mrn-conflict (r/status 503)
-        (throw e)))))
+                         content-type :content-type
+                         :as request}]
+  (let [params (case content-type
+                 "application/x-www-form-urlencoded" (request :params)
+                 "application/json" (request :body)
+                 nil)]
+    (try
+      (-> (svc/create! fhir-server-url params)
+          (r/response)
+          (r/status 201))
+      (catch Exception e
+        (case (:type (ex-data e))
+          :invalid-params (r/status 400)
+          :mrn-conflict (r/status 503)
+          (throw e))))))
 
 (defroutes handler
   (POST "/" _ create-patient!))
