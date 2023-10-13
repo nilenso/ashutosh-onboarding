1a61833ae72fdfeb0bd270972860f73148769127
Ashutosh Gangwar <ashutosh@nilenso.com>
2023-10-13 20:00:03 +0530

    clinic: add route handlers for front-end resources and  wrap-params middleware

--- clinic/src/clj/clinic/routes/core.clj
+++ clinic/src/clj/clinic/routes/core.clj
@@ -2,15 +2,21 @@
   (:require [clinic.config :as config]
             [clinic.routes.patient :as patient]
             [clojure.tools.logging :as log]
-            [compojure.core :refer [context defroutes]]
-            [compojure.route :refer [not-found]]
+            [compojure.core :refer [context defroutes GET]]
+            [compojure.route :refer [not-found resources]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :as r]))
 
 (defroutes ^:private routes
   (context "/api/v1" _
-    (context "/patients" _ patient/handler))
-  (not-found (r/status 404)))
+    (context "/patients" _ patient/handler)
+    (not-found (r/status 404)))
+
+  ;; UI
+  (resources "/")
+  (GET "/*" _ (r/resource-response "index.html" {:root "public"})))
 
 (defn- wrap-exception-handler [next-handler]
   (fn exception-handler [request]
@@ -23,6 +29,8 @@
   "The default API route handler."
   (-> routes
       (config/wrap)
+      (wrap-keyword-params)
+      (wrap-params)
       (wrap-json-body {:keywords? true})
       (wrap-json-response)
       (wrap-exception-handler)))
